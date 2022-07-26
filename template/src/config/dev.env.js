90ad4ea9d24fa5d41324206c381e2505269be94d
wrfighting <skycitywei@sina.com>
2022-07-26 23:01:00 +0800

    feat:优化间距

--- template/src/config/dev.env.js
+++ template/src/config/dev.env.js
@@ -8,12 +8,12 @@ module.exports = {
 {% if redis %}
     redis_options: {
         cluster: false,
-            port: 6379, // Redis port
-            host: '127.0.0.1', // Redis host
-            family: 4, // 4 (IPv4) or 6 (IPv6)
-            password: 'admin',
-            db: 0,
-            retryStrategy: function (times) {
+        port: 6379, // Redis port
+        host: '127.0.0.1', // Redis host
+        family: 4, // 4 (IPv4) or 6 (IPv6)
+        password: 'admin',
+        db: 0,
+        retryStrategy: function (times) {
             const delay = Math.min(times * 50, 2000)
             return delay
         },
@@ -22,26 +22,26 @@ module.exports = {
 {% if mysql %}
     mysql_options: {
         dataBase: 'json-controller-test',
-            account: 'root',
-            pwd: 'admin',
-            options: {
+        account: 'root',
+        pwd: 'admin',
+        options: {
             host: '127.0.0.1',
-                port: 3306,
-                dialect: 'mysql',
-                //logging: false,
-                dialectOptions: {
+            port: 3306,
+            dialect: 'mysql',
+            //logging: false,
+            dialectOptions: {
                 charset: 'utf8mb4',
-                    //collate: "utf8mb4_unicode_ci",
-                    supportBigNumbers: true,
+                //collate: "utf8mb4_unicode_ci",
+                supportBigNumbers: true,
             },
             define: {
                 charset: 'utf8mb4',
             },
             pool: {
                 max: 5,
-                    min: 0,
-                    acquire: 30000,
-                    idle: 10000,
+                min: 0,
+                acquire: 30000,
+                idle: 10000,
             },
             timezone: '+08:00',
         },
